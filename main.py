da0e8072e830608cdded3b23459b7a32db18a000
bdachung <bdh240901@gmail.com>
2022-04-07 15:01:31 +0700

    update main.py

--- main.py
+++ main.py
@@ -8,11 +8,37 @@ from selenium.webdriver.firefox.options import Options
 from selenium.webdriver.support.wait import WebDriverWait
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support import expected_conditions as EC
+import serial.tools.list_ports
+
+mess = ""
+bbc_port = ""
+if len(bbc_port) > 0:
+    ser = serial.Serial(port=bbc_port, baudrate=115200)
+
+def processData(data):
+    data = data.replace("!", "")
+    data = data.replace("#", "")
+    splitData = data.split(":")
+    print(splitData)
+
+def readSerial():
+    bytesToRead = ser.inWaiting()
+    if (bytesToRead > 0):
+        global mess
+        mess = mess + ser.read(bytesToRead).decode("UTF-8")
+        while ("#" in mess) and ("!" in mess):
+            start = mess.find("!")
+            end = mess.find("#")
+            processData(mess[start:end + 1])
+            if (end == len(mess)):
+                mess = ""
+            else:
+                mess = mess[end+1:]
 
 print("Hello ThingBoard")
 
-FOLDER = "C:/Users/asus/AppData/Roaming/Mozilla/Firefox/Profiles"
-PROFILE_NAME = "pmr27fcv.default-1642918215471"
+FOLDER = "/home/dh2409/.mozilla/firefox/"
+PROFILE_NAME = "10pya2yy.default-release"
 driver = None
 
 
@@ -30,7 +56,6 @@ def setup():
 
 
 def get_latlng(lat_default, lng_default):
-    global driver
     try:
         findbtn = WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.ID, "find-loc")))
         findbtn.click()
@@ -54,12 +79,16 @@ def subscribed(client, userdata, mid, granted_qos):
 
 def recv_message(client, userdata, message):
     print("Received: ", message.payload.decode("utf-8"))
-    temp_data = {'value': True}
+    led_data = {}
+    pump_data = {}
     try:
         jsonobj = json.loads(message.payload)
-        if jsonobj['method'] == "setValue":
-            temp_data['value'] = jsonobj['params']
-            client.publish('v1/devices/me/attributes', json.dumps(temp_data), 1)
+        if jsonobj['method'] == "setLED":
+            led_data['valueLED'] = jsonobj['params']
+        client.publish('v1/devices/me/attributes', json.dumps(led_data), 1)
+        if jsonobj['method'] == "setPUMP":
+            pump_data['valuePUMP'] = jsonobj['params']
+        client.publish('v1/devices/me/attributes', json.dumps(pump_data), 1)
     except:
         pass
 
@@ -86,7 +115,8 @@ setup()
 temp = 30
 humidity = 50
 light_intensity = 0
-latitude, longitude = get_latlng(10.8231, 106.6297)
+latitude, longitude = 0,0
+# latitude, longitude = get_latlng(10.8231, 106.6297)
 counter = 0
 
 
